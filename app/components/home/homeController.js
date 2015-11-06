4c0cfc5b1824a3aa7687c19898a903b7f4af9c97
jhorzyto <jhorzyto@gmail.com>
2015-11-06 13:59:16 -0300

    Melhorias no cache da pagina

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -5,7 +5,7 @@ margPhotos.controller( 'HomeController', function ( $scope, CoreService ) {
     $scope.teste = "HomeController";
     $scope.imagePath = "https://material.angularjs.org/latest/img/washedout.png";
 
-    CoreService.getInformation({ params: { feed: 10 }}, function( response ){
+    CoreService.getInformation({ params: { feed: 50 }}, function( response ){
         CoreService.processResponse( response, function( data ){
             CoreService.validateLogin( data );
             nextUrl = data.data.pagination.next_url;
