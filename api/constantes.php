bf5238aa72ff5341b31427c1a5844df7f921e230
jhorzyto <jhorzyto@gmail.com>
2015-12-28 17:11:22 -0300

    NãoVersionado

--- api/constantes.php
+++ api/constantes.php
@@ -2,4 +2,4 @@
 
 define('apiKey','fd57d9161ab5441aa5cac8826c3c0d5e');
 define('apiSecret','5a9d3b8268204770a9a359a91e3ca346');
-define('apiCallback','http://margphotos.esy.es/api/success.php');
+define('apiCallback','http://localhost/margphotos/api/success.php');
